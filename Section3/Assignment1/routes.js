e993dc4334ae1d0e5c8a8e1ec9559ab849cb3509
599746 <abhijitkhanna@C02WW36LHTDD.local>
2020-02-03 18:05:24 -0500

    completing assignment to demonstrate learning of Node basis, creating a server, routing, etc.

--- Section3/Assignment1/routes.js
+++ Section3/Assignment1/routes.js
@@ -8,8 +8,24 @@ const requestHandler = (req, res) => {
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Udemy NodeJS Homework Assignment 1</title></head>');
-        res.write('<body><h1>Welcome! Enjoy your learning journey!</h1></body>')
+        res.write('<body><form action="/create-user" method="POST"><button type ="submit">User Name</button><input type="text" name="username"></form></body>')
         res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/create-user' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody=Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            console.log(message);
+        })
+        //res.write('<body><h1>Welcome! Enjoy your learning journey!</h1></body>')
+        res.statusCode=302;
+        res.setHeader('Location', '/');
         return res.end();
     }
 
